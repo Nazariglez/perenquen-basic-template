794b80ad52d9cfc0c2fa06cd200585ad6b6f6e24
Nazariglez <nazari.nz@gmail.com>
2015-04-01 00:07:54 +0100

    minor fix

--- template/src/index.js
+++ template/src/index.js
@@ -1,7 +1,10 @@
-var plugin = require('./Plugin'),
-    config = require('./config'),
+var config = require('./config'),
+    plugin = require('./Plugin'),
     Main = require('./game/Main');
 
 plugin.enable(config.plugins);
+
+PQ.config.useDeltaAnimation = config.useDeltaAnimation;
+PQ.config.useSortChildrenByDepth = config.useSortChildrenByDepth;
 
 module.exports = new Main();
